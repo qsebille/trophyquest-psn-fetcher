e833898e4974023152b05694a94a1cdf1249fbde
qsebille <quentin.sebille@gmail.com>
2026-01-28 10:02:25 +0100

    Fix avatar for players (#41)

--- src/psn/helpers/get-user-profile.ts
+++ src/psn/helpers/get-user-profile.ts
@@ -8,9 +8,13 @@ export async function getUserProfile(
     const {getProfileFromUserName} = await import("psn-api");
 
     const userPsn = await getProfileFromUserName(auth, profileName);
+    const avatarUrls = userPsn.profile.avatarUrls;
+    const avatarUrl = avatarUrls[avatarUrls.length - 1]?.avatarUrl ?? null;
+    const profilePictureUrls = userPsn.profile.personalDetail?.profilePictureUrls ?? [];
+    const profilePictureUrl = profilePictureUrls[profilePictureUrls.length - 1]?.profilePictureUrl ?? null;
     return {
         id: userPsn.profile.accountId,
         pseudo: userPsn.profile.onlineId,
-        avatarUrl: userPsn.profile.avatarUrls[0]?.avatarUrl ?? null
+        avatarUrl: profilePictureUrl ?? avatarUrl ?? null
     } as Player;
 }
